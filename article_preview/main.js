77833d0ef2f18f03e351ca19e03c2790610cce70
unknown <tommaso.duranti191@gmail.com>
2026-01-15 11:15:17 +0100

    Complete tablete design

--- article_preview/main.js
+++ article_preview/main.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const profile = document.getElementById("profile");
     const toast = document.getElementById("toast");
-
     let iconShareButtons = document.querySelectorAll(".icon_share");
+    
     iconShareButtons.forEach(iconShareButton => {
         iconShareButton.addEventListener("click", () => {
             profile.classList.toggle("profile--is_hidden");
