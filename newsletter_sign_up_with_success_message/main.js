fad1454ee1dae9ed2cb89127e8e1ad1b3a353112
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-01-20 23:25:09 +0100

    Begin error validation

---
+++ newsletter_sign_up_with_success_message/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("form");
+    const email = document.getElementById("email");
+    const submitButton = document.getElementById("submit");
+
+    form.addEventListener( e => {
+        e.preventDefault();
+    })
+    
+})
