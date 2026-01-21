27def09be6571742b5bca6d829a681cc32fe4101
unknown <tommaso.duranti191@gmail.com>
2026-01-21 12:26:39 +0100

    Complete form validation

--- newsletter_sign_up_with_success_message/main.js
+++ newsletter_sign_up_with_success_message/main.js
@@ -1,10 +1,28 @@
 window.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("form");
     const email = document.getElementById("email");
+    const error = document.getElementById("error");
     const submitButton = document.getElementById("submit");
+    const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;
 
-    form.addEventListener( e => {
-        e.preventDefault();
+    function isValidEmail() {
+        const validity = email.value.length !== 0 && emailRegExp.test(email.value);
+        return validity;
+    };
+
+    email.addEventListener("input", () => {
+        email.classList.contains("form__email--is_invalid") ? email.classList.remove("form__email--is_invalid") : false;
+        error.classList.contains("is_hidden") ? false : error.classList.add("is_hidden");
+        isValidEmail()? submitButton.classList.add("form__submit--is_valid"): submitButton.classList.remove("form__submit--is_valid");
     })
-    
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (isValidEmail()) {
+            submitButton.classList.contains("form__submit--is_valid")? submitButton.classList.remove("form__submit--is_valid"): false;
+        } else {
+            email.classList.add("form__email--is_invalid");
+            error.classList.remove("is_hidden");
+        }
+    })
 })
