161068188467ff23058a8ba8d3ea588215c25745
unknown <tommaso.duranti191@gmail.com>
2026-01-28 18:36:00 +0100

    Complete desktop design

--- time_tracking_dashboard/js/main.js
+++ time_tracking_dashboard/js/main.js
@@ -1,10 +1,14 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const currentHours = document.querySelectorAll(".current_hours");
+    const avatarName = document.getElementById("avatar_name");
+
+    const tabletSize = window.matchMedia("(min-width: 48rem)");
+    const desktopSize = window.matchMedia("(min-width: 90rem)");
 
     window.addEventListener("resize", () => {
 
-        if (window.matchMedia("(min-width: 48rem)").matches) {
+        if (tabletSize.matches) {
             currentHours.forEach(currentHour => {
                 currentHour.classList.replace("text_preset_3", "text_preset_1");
             })
@@ -12,6 +16,12 @@ document.addEventListener("DOMContentLoaded", () => {
             currentHours.forEach(currentHour => {
                 currentHour.classList.replace("text_preset_1", "text_preset_3");
             })
+        }
+
+        if (desktopSize.matches) {
+            avatarName.classList.replace("text_preset_4", "text_preset_2");
+        } else{
+            
         }
 
     })
